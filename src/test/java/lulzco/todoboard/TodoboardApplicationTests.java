f70fa4fb184c0d0d4a460de0d11d73d1ff50bd9e
LulzCo <ysw991106@gmail.com>
2023-05-15 16:40:00 +0900

    set

---
+++ src/test/java/lulzco/todoboard/TodoboardApplicationTests.java
@@ -0,0 +1,13 @@
+package lulzco.todoboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
