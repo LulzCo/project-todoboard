d78a93aba9930ed119a54975bb1a642e958e3489
LulzCo <ysw991106@gmail.com>
2023-06-06 20:29:41 +0900

    tag controller create 함수만 생성함

---
+++ src/main/java/lulzco/todoboard/issue/tag/TagController.java
@@ -0,0 +1,23 @@
+package lulzco.todoboard.issue.tag;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/tag")
+public class TagController {
+
+    private final TagService tagService;
+
+    @Autowired
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    @PostMapping("/create")
+    public void create(@RequestBody Tag tag) {
+        tagService.create(tag);
+    }
+}
