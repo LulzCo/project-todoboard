3296c91c498391bd457fbacdb24e4b74b744c3c4
LulzCo <ysw991106@gmail.com>
2023-06-07 20:31:51 +0900

    tagController 기본 crud 구현 완료

--- src/main/java/lulzco/todoboard/issue/tag/TagController.java
+++ src/main/java/lulzco/todoboard/issue/tag/TagController.java
@@ -1,10 +1,11 @@
 package lulzco.todoboard.issue.tag;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/tag")
 public class TagController {
@@ -17,7 +18,26 @@ public class TagController {
     }
 
     @PostMapping("/create")
-    public void create(@RequestBody Tag tag) {
+    public ResponseEntity<String> create(@RequestBody Tag tag) {
         tagService.create(tag);
+        return ResponseEntity.status(HttpStatus.OK).body("태그 생성 완료");
+    }
+
+    @GetMapping("/read/{userId}")
+    public ResponseEntity<List<Tag>> getTagByUserId(@PathVariable("userId") String userId) {
+        List<Tag> tags = tagService.getTagByUserId(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(tags);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<String> update(@RequestBody Tag tag) {
+        tagService.updateTagName(tag.getId(), tag.getTagName());
+        return ResponseEntity.status(HttpStatus.OK).body("태그 수정 완료");
+    }
+
+    @DeleteMapping("/delete/{tagId}")
+    public ResponseEntity<String> delete(@PathVariable("tagId") Long tagId) {
+        tagService.delete(tagId);
+        return ResponseEntity.status(HttpStatus.OK).body("태그 삭제 완료");
     }
 }
