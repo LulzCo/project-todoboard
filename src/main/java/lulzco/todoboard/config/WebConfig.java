fdc934e00e2472d325b8a11152be97c5c9219517
LulzCo <ysw991106@gmail.com>
2023-06-11 15:48:26 +0900

    cors 문제 해결

--- src/main/java/lulzco/todoboard/config/WebConfig.java
+++ src/main/java/lulzco/todoboard/config/WebConfig.java
@@ -1,2 +1,15 @@
-package lulzco.todoboard.config;public class WebConfig {
+package lulzco.todoboard.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+//        WebMvcConfigurer.super.addCorsMappings(registry);
+    }
 }
