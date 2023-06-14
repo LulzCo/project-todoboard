e5eed50dea14e45f0cc1e9095f2162574389130a
LulzCo <ysw991106@gmail.com>
2023-06-14 15:42:09 +0900

    이슈 상세 화면 데이터 조회 성공

--- client/public/src/board/issue.js
+++ client/public/src/board/issue.js
@@ -1,12 +1,26 @@
 // issue.html
 // URL 매개변수 추출
-const urlParams = new URLSearchParams(window.location.search);
-const data = urlParams.get('data');
-console.log(data);
 
-// 데이터 사용
-//document.getElementById('taskName').innerText = taskName;
+setIssue();
 
+function setIssue() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let data = JSON.parse(urlParams.get('data'));
+
+    document.getElementById("title").value = data.title;
+    document.getElementById("tag").value = data.tagName;
+    document.getElementById("status").value = data.status;
+    document.getElementById("contents").value = data.contents;
+    document.getElementById("createdAt").innerText = timeToString(data.createdAt);
+    document.getElementById("updatedAt").innerText = timeToString(data.updatedAt);
+    document.getElementById("dueType").value = data.dueType;
+    document.getElementById("dueDate").value = timeToString(data.dueDate);
+
+}
+
+function timeToString(time) {
+    return time.substring(0, 4) + "년 " + time.substring(5, 7) + "월 " + time.substring(8, 10) + "일 " + time.substring(11, 13) + "시 " + time.substring(14, 16) + "분";
+}
 
 function editIssue() {
   const issueFields = document.getElementsByClassName('field-value');
